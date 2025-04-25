9814a5ca24f82bd866a03606af110d27626427e9
peroperoaa <a3046229569@163.com>
2025-04-25 20:21:36 +0800

    Build a test map and create some code

---
+++ Source/PVZ/Public/TypicalPlantAttackComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlantAttackBaseComponent.h"
+#include "TypicalPlantAttackComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PVZ_API UTypicalPlantAttackComponent : public UPlantAttackBaseComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/PVZ/Public/TypicalPlantHealthComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlantHealthBaseComponent.h"
+#include "TypicalPlantHealthComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PVZ_API UTypicalPlantHealthComponent : public UPlantHealthBaseComponent
+{
+	GENERATED_BODY()
+	
+};
